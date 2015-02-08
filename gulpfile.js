8cbbff9890636191c1fc245a27b7e0abdd5d137e
Humakt83 <urlact@yahoo.com>
2015-02-08 11:26:05 +0200

    Collector: gulbing and bowering

---
+++ gulpfile.js
@@ -0,0 +1,18 @@
+'use strict';
+
+var gulp = require('gulp');
+
+var browserSync = require('browser-sync');
+var reload = browserSync.reload;
+
+gulp.task('default', ['serve'], function() {});
+
+gulp.task('serve', function() {
+  browserSync({
+    server: {
+      baseDir: 'app'
+    }
+  });
+
+  gulp.watch(['*.html', 'styles/**/*.css', 'scripts/**/*.js'], {cwd: 'app'}, reload);
+});
