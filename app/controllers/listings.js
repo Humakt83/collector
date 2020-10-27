1dc85108fdc032242d9ed8044e6a64bae24750e1
Humakt83 <urlact@yahoo.com>
2020-10-27 02:26:57 +0200

    Listings route to pictures

--- app/controllers/listings.js
+++ app/controllers/listings.js
@@ -1,9 +1,13 @@
 'use strict'
 
-angular.module('collector').controller('ListingsController', ['$scope', '$http', function($scope, $http) {
+angular.module('collector').controller('ListingsController', ['$scope', '$http', '$location', function($scope, $http, $location) {
     
 	$http.get('res/lists/special.json').then(function(result) {
         $scope.specials = result.data;
     });
+
+    $scope.goToPicture = function(query) {
+        $location.path('pictures').search({query});
+    }
     
 }]);
