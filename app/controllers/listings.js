4593bc63ac6a477a6895f2023b82dba5fa7b280d
Humakt83 <urlact@yahoo.com>
2020-10-27 03:13:52 +0200

    Add some more items to listings

--- app/controllers/listings.js
+++ app/controllers/listings.js
@@ -2,11 +2,17 @@
 
 angular.module('collector').controller('ListingsController', ['$scope', '$http', '$location', function($scope, $http, $location) {
     
-	$http.get('res/lists/special.json').then(function(result) {
+	$http.get('res/lists/special.json').then(function(result) {        
+        result.data.sort(function (a, b) { 
+            return a.title.localeCompare(b.title);
+        });
         $scope.specials = result.data;
     });
 
     $scope.goToPicture = function(query) {
+        if (!query) {
+            return;
+        }
         $location.path('pictures').search({query});
     }
     
